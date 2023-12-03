e011e8f8cb1f49e4474c29baae7dec5d4bea1ba5
ShayCohenn <shay91847@gmail.com>
2023-12-03 02:24:10 +0200

    build fix

--- components/custom-components/Item.tsx
+++ components/custom-components/Item.tsx
@@ -2,7 +2,7 @@
 
 import { endpoints, iconMapping } from "@/constants";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { ChevronDown, ChevronRight, FileQuestion, LucideIcon } from "lucide-react";
 import { useState } from "react";
 
 interface ItemPorps {
@@ -35,7 +35,8 @@ const Item = ({
           <span>{label}</span>
           {Object.keys(endpoints).map((key) => {
             const Icon = key === label && iconMapping[key];
-            return Icon ? <Icon key={key} className="h-5 w-5 ml-2"/> : null;
+            if(Icon) return <Icon key={key} className="h-5 w-5 ml-2"/>
+            else return <FileQuestion key={key} className="h-5 w-5 ml-2"/>
           })}
         </div>
       ) : (
